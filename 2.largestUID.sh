6eedbd35bec4aa0362bb103c7096bb43c1a5a7db
Nikhil <nikhilus@hotmail.com>
2018-01-28 00:11:49 -0600

    largestUID

--- 2.largestUID.sh
+++ 2.largestUID.sh
@@ -3,4 +3,6 @@
 # Data:         1/Jan/2018
 # Description:  This is showcasing the diffrence between python and bash to largest UID
 # Reference:    Check 1.maxuid.py for reference 
+
+
 sort -n -t: -k3 /etc/passwd | tail -1 | cut -d: -f3 
