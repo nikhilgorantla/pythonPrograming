ce8752d9fad170557c45004965e4a92d084d3995
Nikhil <nikhilus@hotmail.com>
2018-01-28 00:11:13 -0600

    largestUID

--- 2.largestUID.sh
+++ 2.largestUID.sh
@@ -2,5 +2,5 @@
 # Author:       Nikhil Gorantla
 # Data:         1/Jan/2018
 # Description:  This is showcasing the diffrence between python and bash to largest UID
-
+# Reference:    Check 1.maxuid.py for reference 
 sort -n -t: -k3 /etc/passwd | tail -1 | cut -d: -f3 
--- 9.dirlist.py
+++ 9.dirlist.py
@@ -7,3 +7,10 @@ dirName = input("Enter the Directory name to list the size of the files: ")
 for file in os.listdir(dirName):
     info = os.stat(file)
     print("%-20s : size %d" % (file, info.st_size))
+
+
+# file:         largestUID.sh
+# Author:       Nikhil Gorantla
+# Data:         1/Jan/2018
+# Description:  This is showcasing the diffrence between python and bash to largest UID
+# Reference:    Check 1.maxuid.py for reference
