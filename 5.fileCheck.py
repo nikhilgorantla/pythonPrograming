ec810f9c9df99b216aca4dcbc24b166e209c9d82
Nikhil <nikhilus@hotmail.com>
2018-01-28 00:16:31 -0600

    fileCheck-v2

--- 5.fileCheck.py
+++ 5.fileCheck.py
@@ -1,8 +1,13 @@
-# More Pythonic way of file checking 
+# file:         fileCheck.py
+# Author:       Nikhil Gorantla
+# Data:         1/Jan/2018
+# Description:  More Pythonic way of file checking 
+
+
 #!/usr/bin/python3  
 
 try:
     f = open("/etc/hosts")
-    #go ahead and read the file 
+    # go ahead and read the file 
 except FileNotFoundError:
     print("no hostsfile")
