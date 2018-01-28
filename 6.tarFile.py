33b16578fd4169f9b88d519c8df97ff1f058608d
Nikhil <nikhilus@hotmail.com>
2018-01-28 00:19:23 -0600

    tarFile

--- 6.tarFile.py
+++ 6.tarFile.py
@@ -1,5 +1,12 @@
+# file:         tarFile.py
+# Author:       Nikhil Gorantla
+# Data:         1/Jan/2018
+# Description:  This program will tar the file's in the given directory
+
+
+
 import tarfile          #this module will help to tar files 
-import glob             #module finds all the pathnames matching a specified pattern according to the rules used by the Unix shell, although results are returned in arbitrary order
+import glob             #module finds all the path names matching a specified pattern according to the rules used by the Unix shell, although results are returned in arbitrary order
 
 filePath = input("enter the file path: ")
 
